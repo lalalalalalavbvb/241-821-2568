745a29ff71129f2d09d8d54232498fad18b62633
lalalalalalavbvb <apisit.thats@ku.th>
2026-03-10 11:18:44 +0700

    add .

--- Backend/index.js
+++ Backend/index.js
@@ -94,30 +94,57 @@ app.get('/users/:id', async (req, res) =>
     res.json(user);
 });*/
 
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastname) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจอย่างน้อย 1 อย่าง');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบายเกี่ยวกับตัวคุณ');
+    }
+    return errors;
+}
+
+
 // path = POST /users (POST เพิ่ม) สำหรับเพิ่ม user ใหม่
-app.post('/users', async (req, res) =>
-{
-    try
-    {
+app.post('/users', async (req, res) => {
+    try {
         let user = req.body;
-        const results = await conn.query('INSERT INTO users SET ?', user);
-        console.log('results:', results);
-        res.json
-        ({
-        message: 'User created successfully',
-        data: results[0]
+        const errors = validateData(user);
+        if (errors.length > 0) {
+            //ถ้ามี error 
+            throw {
+                message: 'กรอกข้อมูลไม่ครบถ้วน',
+                errors: errors  
+            }
+        }
+        const results = await conn.query('INSERT INTO users SET ?', user)
+        res.json({
+            message: 'User created successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        const errorMessage = error.message || 'Error creating user';
+        const errors = error.errors || [];
+        console.error('Error creating user:', error.message);
+        res.status(500).json({
+            message: errorMessage,
+            errors: errors
         });
     }
-    catch (error)
-    {
-        console.error('Error creating user: ' , error);
-        res.status(500).json
-            ({
-                message: 'Error creating user',
-                error: error.message
-            });
-    }    
-})
+});
 
 // path = PUT /user/:id (PUT แก้ | PATCH แก้อย่างเดียว มั้ง!)
 /*app.patch('/user/:id', (req, res) =>
